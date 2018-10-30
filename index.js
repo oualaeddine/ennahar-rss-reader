e8c7df5933caaf2481354bdeea17735e556f6574
berrehal ouala eddine <berrehaloualaeddine@gmail.com>
2018-10-30 15:24:50 +0100

    dependencies

--- index.js
+++ index.js
@@ -1,44 +1,26 @@
-const express = require('express');
 const Parser = require('rss-parser');
 const admin = require('firebase-admin');
 
 // Initialize Firebase
-
-
-var serviceAccount = require("./serviceAccountKey.json");
-
+const serviceAccount = require("./serviceAccountKey.json");
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://ultimatefacebookhackerv2372017.firebaseio.com"
 });
 
+let parser = new Parser();
+(async () => {
 
-const PORT = process.env.PORT || 5000;
+    let feed = await parser.parseURL('https://www.ennaharonline.com/feed/');
+    feed.items.forEach(item => {
+        saveItem(item);
+    });
 
+})();
 
-let app = express();
-
+const db = admin.database();
 function saveItem(item) {
-    // Get a database reference to our blog
-    var db = admin.database();
-
-    var myDate = new Date(item.isoDate);
-
-    var ref = db.ref("rss/" + myDate.getTime());
+    let myDate = new Date(item.isoDate);
+    let ref = db.ref("rss/" + myDate.getTime());
     ref.set(item);
-    console.info(item);
-
 }
-
-//app.get('/', (req, res) => {
-    let parser = new Parser();
-
-    (async () => {
-
-        let feed = await parser.parseURL('https://www.ennaharonline.com/feed/');
-        feed.items.forEach(item => {
-            saveItem(item);
-        });
-
-    })();
-//}).listen(PORT, () => console.log(`Listening on ${PORT}`));
